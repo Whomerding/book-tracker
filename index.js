beb8a8993a02cbfef4eda3a037a8ba81324e0f43
Whomerding <wendy.homerding@gmail.com>
2026-01-21 09:09:57 -0500

    add search.ejs file, partials, header.ejs, seed API data.

---
+++ index.js
@@ -0,0 +1,137 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+db.connect();
+const books_search_data=[
+    {
+        "id": "6596.The_Four_Agreements",
+        "title": "The Four Agreements: A Practical Guide to Personal Freedom",
+        "author": "Miguel Ruiz",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1630664059l/6596._SY500_.jpg",
+        "rating": "4.19"
+    },
+    {
+        "id": "214151420-the-house-of-my-mother",
+        "title": "The House of My Mother: A Daughter's Quest for Freedom",
+        "author": "Shari Franke",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1728843692l/214151420._SY500_.jpg",
+        "rating": "4.31"
+    },
+    {
+        "id": "7905092-freedom",
+        "title": "Freedom",
+        "author": "Jonathan Franzen",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1316729686l/7905092._SY500_.jpg",
+        "rating": "3.80"
+    },
+    {
+        "id": "318431.Long_Walk_to_Freedom",
+        "title": "Long Walk to Freedom",
+        "author": "Nelson Mandela",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1327997342l/318431._SY500_.jpg",
+        "rating": "4.36"
+    },
+    {
+        "id": "24611623-in-order-to-live",
+        "title": "In Order to Live: A North Korean Girl's Journey to Freedom",
+        "author": "Yeonmi Park",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1437799970l/24611623._SY500_.jpg",
+        "rating": "4.48"
+    },
+    {
+        "id": "34964905-the-sun-does-shine",
+        "title": "The Sun Does Shine: How I Found Life and Freedom on Death Row",
+        "author": "Anthony Ray Hinton",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1591203630l/34964905._SY500_.jpg",
+        "rating": "4.65"
+    },
+    {
+        "id": "9021.Long_Walk_to_Freedom",
+        "title": "Long Walk to Freedom: Volume 1",
+        "author": "Nelson Mandela",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348440647l/9021._SY500_.jpg",
+        "rating": "4.37"
+    },
+    {
+        "id": "11797365-escape-from-camp-14",
+        "title": "Escape from Camp 14: One Man's Remarkable Odyssey from North Korea to Freedom in the West",
+        "author": "Blaine Harden",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347954566l/11797365._SY500_.jpg",
+        "rating": "4.01"
+    },
+    {
+        "id": "25330108-freedom-is-a-constant-struggle",
+        "title": "Freedom is a Constant Struggle: Ferguson, Palestine and the Foundations of a Movement",
+        "author": "Angela Y. Davis",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1447140494l/25330108._SY500_.jpg",
+        "rating": "4.44"
+    },
+    {
+        "id": "56132724-the-woman-they-could-not-silence",
+        "title": "The Woman They Could Not Silence: One Woman, Her Incredible Fight for Freedom, and the Men Who Tried to Make Her Disappear",
+        "author": "Kate Moore",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1614685052l/56132724._SY500_.jpg",
+        "rating": "4.30"
+    }
+]
+console.log("Book search data loaded:", books_search_data[0].image);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");});
+
+app.get("/search", (req, res) => { 
+    const query = req.query.book;
+    const results = books_search_data
+    console.log('search results:', results);
+    res.render("search.ejs", { results: results, query: query });
+});
+
+
+app.post("/add", async (req, res) => {
+  console.log("request body:", req.body);
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+    const data = result.rows[0];
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [data.country_code, currentUserId]  
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
